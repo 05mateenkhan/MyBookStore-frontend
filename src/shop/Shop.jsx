f6dfcc19a100beab84923c067ff4f65752ca6f00
05mateenkhan <05mateenkhan@gmail.com>
2025-04-14 20:44:53 +0530

    book description

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -28,7 +28,7 @@ export default function Shop() {
                                 <a href={`/book/${book._id}`}>
                                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{book.bookTitle}</h5>
                                 </a>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.bookDescription.substring(0,100)}...</p>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{book && book.bookDescription && book.bookDescription.substring(0,100)}...</p>
                                 <a href={`/book/${book._id}`} className="w-full flex items-center justify-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Buy now
                                      {/* <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
