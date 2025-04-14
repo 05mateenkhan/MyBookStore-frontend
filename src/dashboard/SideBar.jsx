f91c69ce51022cd63ea9cf5ae703527ff03e5bf2
05mateenkhan <05mateenkhan@gmail.com>
2025-04-14 21:22:47 +0530

    sidebar

--- src/dashboard/SideBar.jsx
+++ src/dashboard/SideBar.jsx
@@ -1,8 +1,13 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import userImage from "../assets/profile.jpg"
 import { AuthContext } from "../contexts/AuthProvider"
 export default function SideBar() {
 
+
+   const [isMenuOpen,setIsMenuOpen] = useState(true);
+   const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+   }
    const {user} = useContext(AuthContext);
    console.log(user);
    let userImg = userImage;
@@ -10,7 +15,8 @@ export default function SideBar() {
    if(user && user.photoURL) userImg = user.photoURL;
     return (
         <div>
-            <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+
+            <button onClick={toggleMenu} data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" className={`${isMenuOpen ? "" : "hidden"} inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm: hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}>
    <span className="sr-only">Open sidebar</span>
    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
    <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
@@ -18,7 +24,7 @@ export default function SideBar() {
 </button>
 
 <aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+   <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800"> 
 
 
    <a href="/" className="flex items-center ps-2.5 mb-5">
